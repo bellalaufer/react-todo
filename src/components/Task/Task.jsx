ebaee177698ab036eff2fe23534d148312d2b184
Bella <bella.kamala@gmail.com>
2023-05-23 16:23:26 +0300

    updated reducer 2

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -31,24 +31,7 @@ function Task({task, completeTask, deleteTask}) {
             Del
           </button>
         </div>
-      </div>
-        // <div>
-        //     <input 
-        //         className="form-check-input" 
-        //         type="checkbox"
-        //         checked={task.completed}          
-        //         onChange={() => completeTask(task.id)}                 
-        //         id={task.id} /> 
-        //       <label 
-        //         className={task.completed ? "form-check-label text-decoration-line-through" : "form-check-label"}
-        //         htmlFor={task.id}>                
-        //         {task.text}
-        //       </label> 
-        //       <button 
-        //       type="button" 
-        //       className="btn btn-outline-dark btn-sm"
-        //       onClick={() => deleteTask(task.id)}>Del</button>
-        // </div>
+      </div>        
     );
 }
 
